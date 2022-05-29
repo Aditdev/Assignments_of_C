8bb557d5ef5f18e5ea318b531ffe4e0a2892a149
Aditdev <92054021+Aditdev@users.noreply.github.com>
2022-05-29 12:33:37 +0530

    new commit

--- Asgn4/asgn4_2.c
+++ Asgn4/asgn4_2.c
@@ -3,17 +3,18 @@
 
 int main()
 {
-    int a, vcount=0;
-    char str[10], i;
+    int a, vcount=0,i;
+    char str[100];
     printf("Enter a string (without space):");
     scanf("%s",str);
     a=strlen(str);
+    printf("The String is %s",str);
     for (i = 0; i < strlen(str); i++)
     {
         if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
             vcount++;
     }
-    printf("There are %d Vowels and %d consonants", vcount,a-vcount);
+    printf("There are %d Vowel(s) and %d consonant(s)", vcount,a-vcount);
 
     return 0;
 }
