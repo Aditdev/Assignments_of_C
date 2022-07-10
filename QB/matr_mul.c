eff6b95d0cec39f62d9497e322b056d6fee34a23
Aditdev <92054021+Aditdev@users.noreply.github.com>
2022-07-10 13:17:37 +0530

    last commit

--- QB/matr_mul.c
+++ QB/matr_mul.c
@@ -5,7 +5,7 @@ int main()
     int k, sum = 0, s[3][3], t[3][3], mul[3][3];
     int i, j;
     // To read the elements of a 3x3 matrix.
-    printf("Enter elements of12 a 3x3 matrix\n");
+    printf("Enter elements of a 3x3 matrix\n");
     for (i = 0; i < 3; i++)
         for (j = 0; j < 3; j++)
             scanf("%d", &s[i][j]);
